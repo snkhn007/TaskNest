3305d16e12dbd89bc5b122114d6871673085793c
Sana <sanasadafkhan2005@gmail.com>
2026-03-26 08:00:41 +0530

    added EJS partials for dynamic title and improved view structure

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res)=>{
     // res.sendFile(path.join(__dirname, 'views', 'home.html'));
-    res.render('home');
+    res.render('home', {title : 'Home'});
 })
 app.get('/addTask', (req, res)=>{
     // res.sendFile(path.join(__dirname, 'views', 'create.html'));  
-    res.render('create');
+    res.render('create', {title : 'Create Task'});
 })
 app.get('/taskBoard', (req, res)=>{
     // res.sendFile(path.join(__dirname, 'views', 'taskBoard.html'));  
-    res.render('taskBoard');
+    res.render('taskBoard', {title : 'Task Board'});
 })
 app.post('/submit', (req, res)=>{
     res.redirect('/');
@@ -26,11 +26,11 @@ app.post('/submit', (req, res)=>{
 // wil be made in future
 app.get('/signIn', (req, res)=>{
     // res.sendFile(path.join(__dirname, 'views', 'signIn.html'));  
-    res.render('signIn');
+    res.render('signIn', {title : 'Sign In Page'});
 })
 app.use((req, res)=>{
     // res.sendFile(path.join(__dirname, 'views', '404.html'));  
-    res.render('404');
+    res.render('404', {title : '404 Page'});
 })
 app.listen(4000, ()=>{
     console.log('Started...')
